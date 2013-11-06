bbf8adc54c8b0646a091f8df4ef8af00fe9758b2
Ayende Rahien <ayende@ayende.com>
2013-11-06 12:16:33 +0000

    Adding default project

---
+++ Asos.Course/Controllers/AbstractController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+
+namespace Asos.Course.Controllers
+{
+	public class AbstractController : Controller
+	{
+		 private static readonly Lazy<IDocumentStore> _documentStoreLazy = new Lazy<IDocumentStore>(() =>
+		 {
+			 var store = new DocumentStore
+			 {
+				 Url = "http://localhost:8787",
+				 DefaultDatabase = "Asos.Course"
+			 };
+			 store.Initialize();
+
+			 IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store);
+
+			 return store;
+		 });
+
+		 public IDocumentStore DocumentStore { get { return _documentStoreLazy.Value; } }
+	}
+}

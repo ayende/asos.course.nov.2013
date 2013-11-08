0c6e61dd1bff602b8733551a18b9d4e91c142107
Ayende Rahien <ayende@ayende.com>
2013-11-08 11:23:25 +0000

    minor

---
+++ Asos.Course/Controllers/ChangesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Media;
+using Asos.Course.Models;
+
+namespace Asos.Course.Controllers
+{
+	public class ChangesController : AbstractController
+	{
+		public object SetupChanges()
+		{
+			DocumentStore.Changes()
+				.ForAllDocuments()
+				.Subscribe(notification => new SoundPlayer(@"http://www.shockwave-sound.com/sound-effects/scream-sounds/2scream.wav").Play());
+
+			return Json("ok");
+		}
+
+		public object Caching()
+		{
+			using (DocumentStore.AggressivelyCache())
+			{
+				return Json(Session.Load<Workflow>(1));
+			}
+		}
+	}
+
+}
